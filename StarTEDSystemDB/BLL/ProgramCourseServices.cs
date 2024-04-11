553224e69b76118a1e3dce7188dfc26b4a336cc6
Manuel Alva Barrera <jalvabarrera1@nait.ca>
2024-04-11 13:58:27 -0600

    Initial Commit

---
+++ StarTEDSystemDB/BLL/ProgramCourseServices.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StarTEDSystemDB.DAL;
+using StarTEDSystemDB.Entities;
+
+
+namespace StarTEDSystemDB.BLL
+{
+    public class ProgramCourseServices
+    {
+        private readonly StarTEDContext _context;
+
+        internal ProgramCourseServices(StarTEDContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> list of courses associated with a specific program.</returns>
+        public List<ProgramCourse> GetAllProgramCourses(int id)
+        {
+            return _context.ProgramCourses
+                .Where(pc => pc.ProgramId == id)
+                .Include(pc => pc.Course)              
+               .OrderBy(c => c.Course.CourseName)
+               .ToList();
+        }
+
+        /// <summary>
+        /// Get all Courses by Course ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>all ProgramCourses associated with a specific CourseId</returns>
+        public List<ProgramCourse> GetAllCourses(string id)
+        {
+            return _context.ProgramCourses
+                .Where(c => c.CourseId == id)
+                .Include(c => c.Course)
+                .Include(p => p.Program)
+               .OrderBy(c => c.Course.CourseName)
+               .ToList();
+        }
+
+        /// <summary>
+        /// Get a single ProgramCourse by course ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>a single program associated with a given Id</returns>
+        public ProgramCourse? GetProgramCourseById(int id)
+        {
+            return _context.ProgramCourses
+                .Where(pc => pc.ProgramCourseId == id)
+                .Include(c => c.Course)
+                .FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Update the ProgramCourse Active status associated with a given Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="isActive"></param>
+        public void UpdateProgramCourse(int id, bool isActive)
+        {
+            ProgramCourse UpdatedProgramCourse = _context.ProgramCourses
+                .FirstOrDefault(pc => pc.ProgramCourseId == id);
+            UpdatedProgramCourse.Active = isActive;
+
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ StarTEDSystemWebApp/Components/Pages/CRUD.razor.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Components;
+using StarTEDSystemDB.BLL;
+using StarTEDSystemDB.Entities;
+
+namespace StarTEDSystemWebApp.Components.Pages
+{
+    public partial class CRUD
+    {
+        [Inject]
+        ProgramServices ProgramServices { get; set; }
+
+        [Inject]
+        ProgramCourseServices ProgramCourseServices { get; set; }
+
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+
+        //Required properties
+        [Parameter]
+        public int ProgramCourseId { get; set; }
+        
+        [Parameter]
+        public string CourseName { get; set; }  
+
+
+        public List<StarTEDSystemDB.Entities.Program> Programs { get; set; }
+        public List<ProgramCourse> ProgramCourses { get; set; }
+        
+        private ProgramCourse ProgramCourse { get; set; }
+
+        public List<string> errorList = new List<string>();
+        public string feedback { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            ProgramCourse = new();
+            ProgramCourses = new();
+
+            if (ProgramCourseId != 0)
+            {
+                ProgramCourses = ProgramCourseServices.GetAllProgramCourses(ProgramCourseId);
+                ProgramCourse = ProgramCourseServices.GetProgramCourseById(ProgramCourseId);
+
+                if (ProgramCourse == null)
+                {
+                    errorList.Add($"No course found for id {ProgramCourse.CourseId}");
+                }
+            }
+
+            return base.OnInitializedAsync();
+        }
+    }
+}
